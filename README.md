eaee56898c0524b405cedf409e19d018f7b34ed8
IIvexII <mrh4ck3r1738@gmail.com>
2023-03-25 14:11:27 +0500

    feat: add scraper.py, README.md and requirements.txt

---
+++ README.md
@@ -0,0 +1,27 @@
+# Zameen.com Web Scraper
+
+This is a web scraper built to collect data from Zameen.com, a leading online property portal in Pakistan. The scraper collects detailed information on real estate property listings, such as city, location, price, number of bedrooms and bathrooms, and size of the property in square feet.
+
+## Download Dataset
+
+I have collected data of 4000+ properties that you can download from [Zameen.com Property Dataset Pakistan 2023](https://www.kaggle.com/datasets/muhammadzafeer/zameen-com-property-data-pakistan-2023)
+
+## Usage
+
+To use the scraper, simply run the `scraper.py` script or `scraper.ipynb` for Jupyter nootbook. The scraper will generate a CSV file with the collected data, which can be used for a variety of purposes such as market analysis, trend analysis, and investment research.
+
+## Installation
+
+To install the dependencies for the scraper, run the following command:
+
+```
+pip install -r requirements.txt
+```
+
+## Contributing
+
+Contributions to the project are welcome. If you would like to contribute, please open an issue or submit a pull request.
+
+## Contact
+
+If you have any questions or comments about the scraper, please feel free to contact me at contact@zafeer.pk.
---
+++ scraper.py
@@ -0,0 +1,151 @@
+import requests
+from bs4 import BeautifulSoup
+import decimal
+
+# set the global precision level to 2 decimal places
+decimal.getcontext().prec = 2
+
+# convert crore, lakhs and millions,Arab into numbers with comments
+
+
+def convert_price(price):
+    """
+    Convert crore, lakhs and millions into numbers
+
+    :param price: str
+    :return: float
+    """
+    if price.endswith('Crore'):
+        return round(float(price[:-5]) * 10000000)
+    elif price.endswith('Lakh'):
+        return round(float(price[:-4]) * 100000)
+    elif price.endswith('Million'):
+        return round(float(price[:-7]) * 1000000)
+    elif price.endswith('Arab'):
+        return round(float(price[:-4]) * 1000000000)
+    else:
+        return round(float(price))
+
+# convert kanal, merla, Sq. Yd., S  into square feets
+
+
+def convert_size(size):
+    """
+    Convert kanal merla into sqft
+
+    :param size: str
+    :return: float
+    """
+    if size.endswith('Marla'):
+        return round(float(size[:-5].replace(",", "")) * 225)
+    elif size.endswith('Kanal'):
+        return round(float(size[:-5].replace(",", "")) * 4500)
+    elif size.endswith('Sq. Yd.'):
+        return round(float(size[:-7].replace(",", "")) * 9)
+    else:
+        return round(float(size))
+
+
+def text(tag, datatype="str"):
+    """
+    This function will return the text of the tag.
+
+    :param tag: tag object
+    :param datatype: num or str or price, size
+    :return: price in number or string
+    """
+    if tag is None and datatype == "num":
+        return 0
+    if datatype == "num":
+        try:
+            return int(tag.text.strip())
+        except ValueError:
+            return 0
+    if tag is None and datatype == "str":
+        return ""
+    if datatype == "str":
+        return tag.text.strip()
+    if tag is None and datatype == "price":
+        return 0.0
+    if datatype == "price":
+        return convert_price(tag.text.strip())
+    if tag is None and datatype == "size":
+        return 0.0
+    if datatype == "size":
+        return convert_size(tag.text.strip())
+
+
+def scrap(city, pages_range):
+    house_info = []
+
+    for page_number in range(1, pages_range+1):
+        url = f'https://www.zameen.com/Homes/{city}-{page_number}.html'
+        print(url)
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        house_list = soup.select("main > div > div > div > div > ul > li")
+
+        # store lenght of previous length of house info list
+        prev_len = len(house_info)
+
+        for house in house_list:
+            baths = house.select_one("span[aria-label='Baths']")
+            beds = house.select_one("span[aria-label='Beds']")
+            location = house.select_one("div[aria-label='Location']")
+            price = house.select_one("span[aria-label='Price']")
+            size = house.select_one("div[title]>div > div > span:nth-child(1)")
+
+            if price:
+                house_info.append(
+                    {
+                        "location": text(location),
+                        "price":    text(price, datatype="price"),
+                        "bedrooms":     text(beds, datatype="num"),
+                        "baths":    text(baths, datatype="num"),
+                        "size":     text(size, datatype="size")
+                    }
+                )
+
+        # get out of the loop if the last accessed page
+        # doesnot exist to avoid useless requests because
+        # next pages will not exist as well
+        if len(house_info) == prev_len:
+            break
+
+    return house_info
+
+
+if __name__ == "__main__":
+    house_info = []
+
+    cities = [
+        {'id': 1, 'name': 'Lahore'},
+        {'id': 2, 'name': 'Karachi'},
+        {'id': 3, 'name': 'Islamabad'},
+        {'id': 15, 'name': 'Multan'},
+        {'id': 16, 'name': 'Faisalabad'},
+        {'id': 17, 'name': 'Peshawar'},
+        {'id': 18, 'name': 'Quetta'},
+        {'id': 41, 'name': 'Rawalpindi'},
+        {'id': 36, 'name': 'Murree'},
+        {'id': 327, 'name': 'Gujranwala'},
+        {'id': 1233, 'name': 'Attock'},
+        {'id': 3234, 'name': '2_FECHS'},
+    ]
+
+    for city in cities:
+        house_info.append(
+            {
+                "city": city.get('name'),
+                "info": scrap(f"{city.get('name')}-{city.get('id')}", 20)
+            }
+        )
+
+    with open("zameen.csv", "w") as f:
+        # write csv header
+        f.write("city|location|price|bedrooms|baths|size\n")
+        for house in house_info:
+            for info in house.get('info'):
+                f.write(
+                    f"{house.get('city')}|{info.get('location')}|{info.get('price')}|{info.get('bedrooms')}|{info.get('baths')}|{info.get('size')}\n")
